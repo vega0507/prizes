3cdc240ffaf75e169b3f9645271a4e56cffd1439
vega0507 <violetaesmeraldag@yahoo.com>
2021-05-06 21:28:12 -0600

    agregando action para busqueda de producto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,20 @@
-export const validarLogIn = (usuarioLogueado) => { 
-    console.log("en el action "+usuarioLogueado);
+export const validarLogIn = (usuarioLogueado) => {     
     return { 
         type: 'VALIDAR_LOGIN' , 
         payload: usuarioLogueado 
+    };
+};
+
+export const mostrarRegistroPuntos = (mostrar) => {     
+    return { 
+        type: 'MOSTRAR_REGISTRO_PUNTOS' , 
+        payload: mostrar 
+    };
+};
+
+export const buscarProducto = (codigoProducto) => {     
+    return { 
+        type: 'BUSCAR_PRODUCTO' , 
+        payload: codigoProducto 
     };
 };
